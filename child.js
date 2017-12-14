10bc76b9d7b3db0d5c6800e9d169a714cf341f43
Vansh Khanna <vkhan4@sapient.com>
2017-12-14 16:36:33 +0530

    added http agent for socket connection with the parent server

--- child.js
+++ child.js
@@ -1,3 +1,30 @@
+var http = require('http');
+var agent = new http.Agent({keepAlive: true});
+
+
+
+
+const clientSocket = agent.createConnection ({
+  hostname: '127.0.0.1',
+  port: 8100
+}, ()=>{
+  clientSocket.on('data', (message)=> {
+    console.log('message from parent server=======>', message.toString('utf8'));
+  });
+});
+
+// TODO probably in the LINE #19, only one socket is allowed.
+// TODO more POC needed
+// const options = {
+//   port: 8100,
+//   hostname: '127.0.0.1',
+//   path: '/'
+//   agent: agent
+// };
+//
+// const req = http.request(options);
+// req.end();
+
 console.log('Hi I am child process');
 process.on('message', (data)=>{
   console.log(data);
--- parent.js
+++ parent.js
@@ -1,5 +1,21 @@
 const { spawn } = require('child_process');
 console.log('Hi I am parent process!');
+const http = require('http');
+
+
+const httpserver = http.createServer ((req, res) => {
+  //req.end('hello child');
+  console.log('incoming request (get/post) connection from child process');
+
+});
+httpserver.listen(8100, '127.0.0.1', () => {
+  console.log('parent server is up!');
+});
+
+httpserver.on('connection', (socket) => {  // emitted when a new socket connection is established
+  console.log('incoming socket connection from child process');
+  socket.write('connected');
+});
 
 const child_process = spawn(process.argv[0], ['child.js'],  {
   stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
@@ -7,14 +23,13 @@ const child_process = spawn(process.argv[0], ['child.js'],  {
 });
 
 child_process.on('error', (err) => {
-  console.log('Failed to start subprocess.');
+  console.log('Failed to start subprocess.', err);
 });
 
 child_process.on('message', (data)=> {
   console.log(data);
   setTimeout(()=>{child_process.send('This message is sent from PARENT process!')}, 1300);
-})
-
+});
 
 child_process.stdout.on('data', function (data) {
 console.log(data.toString());
